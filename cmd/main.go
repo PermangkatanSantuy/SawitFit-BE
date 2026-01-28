4cb2d0133ff3a018d31b29ab21c2cff9f10d4c7f
Dimas Ramadhani <dimasramadhani330@gmail.com>
2026-01-28 21:52:27 +0700

    [KAN-22] mengintegrasikan supabase dengan project

---
+++ cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dsn := os.Getenv("DATABASE_URL")
+	config, err := pgxpool.ParseConfig(dsn)
+    if err != nil {
+        log.Fatal("ParseConfig error:", err)
+    }
+
+	conn, err := pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+	defer conn.Close()
+
+	// Example query to test connection
+	var version string
+	if err := conn.QueryRow(context.Background(), "SELECT version()").Scan(&version); err != nil {
+		log.Fatalf("Query failed: %v", err)
+	}
+
+	log.Println("Connected to:", version)
+}
